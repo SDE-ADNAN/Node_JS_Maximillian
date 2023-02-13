f0bc05e82ec68e6dc6823a10ecacd7260a3559b3
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-02-14 00:36:16 +0530

    < wrote code for getting values from the request object in http.createServer() method of nodejs > 29 : understanding requests

--- Section_3:_Understanding_the_Basics/first-nodejs-server/app.js
+++ Section_3:_Understanding_the_Basics/first-nodejs-server/app.js
@@ -3,10 +3,12 @@ const http = require('http')
 // we stored the server into the server variable which is returned by the createServer method on the http module
 
 const server = http.createServer((req,res)=>{
-    console.log(req)
+    console.log("request.url -->  "+req.url )
+    console.log("request.method -->  "+req.method )
+    console.log(req.headers)
     //  here if we use the process.exit() , what it does is that it hard exitted the eventloop which we typically do not do to our server as we want our server to be running .
-    // process.exit()
+    process.exit()
 })
 
 // this below line means the server will listen at the (localhost:3000) whenever a new request is gemerated on the port 3000.
-server.listen(3000)
+server.listen(8080)
