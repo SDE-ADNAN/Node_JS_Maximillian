6745a4085c641a54ec415b58af97070f62f1ba64
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-02-13 08:01:30 +0530

    < used the process.exit() and known about eventloop and its importance > 25 : the node lifecycle and the eventloop

--- Section_3:_Understanding_the_Basics/first-nodejs-server/app.js
+++ Section_3:_Understanding_the_Basics/first-nodejs-server/app.js
@@ -4,6 +4,8 @@ const http = require('http')
 
 const server = http.createServer((req,res)=>{
     console.log(req)
+    //  here if we use the process.exit() , what it does is that it hard exitted the eventloop which we typically do not do to our server as we want our server to be running .
+    // process.exit()
 })
 
 // this below line means the server will listen at the (localhost:3000) whenever a new request is gemerated on the port 3000.
