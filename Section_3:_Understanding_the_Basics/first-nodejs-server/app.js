b1c3866a5073986640c522aad0c01a27ddd1ba53
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-02-23 23:52:52 +0530

    < App.js changes for importing routes handler > 38 : Using the Node Modules System

--- Section_3:_Understanding_the_Basics/first-nodejs-server/app.js
+++ Section_3:_Understanding_the_Basics/first-nodejs-server/app.js
@@ -1,53 +1,14 @@
 const http = require('http') 
-const fs = require("fs") // for file system access
+
+const routes = require('./routes')
+
 
 // we stored the server into the server variable which is returned by the createServer method on the http module
 
-const server = http.createServer((req,res)=>{
- const url = req.url
- const method = req.method
-
-
- if(url === '/'){
-    res.write('<html>')
-    res.write('<head><title>Enter Message</title></head>')
-    // the action attr will submit this form and will push the url to domain/message and the method attr will specify the request method as post and the name attr in input is very special as it will append the input value in request.message which we can use to process some further operations.
-    res.write('<body><form action="/message" method="POST" ><input name="message" type="text"></input><button>Send</button></form></body>')
-    res.write('</html>')
-   // we must return here the res.end() as we dont want to continue any further in this function or else it will also continue to run rest of the func.
-   return res.end()
- }
- if(url === '/message' && method === 'POST'){
-   const body =[];
-// this below on data function in called by nodejs when ever it has some thing in the requests data stream and calls this function for each individual chunk of the whole data. 
-   req.on('data', (chunk)=>{
-     console.log(chunk);
-     body.push(chunk);
-   })
-   // this on end is called when each chunk got processed and at the end it writes to the file.
-   return req.on('end',()=>{
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt',message,(err)=>{
-         res.statusCode = 302;
-         res.setHeader('Location','/')
-         return res.end();
-      });
-      })
-
- }
-    //  here if we use the process.exit() , what it does is that it hard exitted the eventloop which we typically do not do to our server as we want our server to be running .
-    // this setHeader sets the type of response we will be giving back in reponse
-    res.setHeader('Content-Type','text/html');
-    // this res.write() method is used to write the response line by line
-    res.write('<html>')
-    res.write('<head><title>My first Page</title></head>')
-    res.write('<body><h1>Hey !! welcome to nodejs</h1></body>')
-    res.write('</html>')
-    // once we used the res.end method we now cant use the res.write method or else it will throw unwanted errors.
-    res.end()
-    // process.exit()
-})
+// if routes is an object exported.
+const server = http.createServer(routes.handler)
+// or else if its a single value.
+// const server = http.createServer(routes)
 
 // this below line means the server will listen at the (localhost:3000) whenever a new request is gemerated on the port 3000.
 server.listen(8080)
