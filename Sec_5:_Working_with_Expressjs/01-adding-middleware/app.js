56dcd72a4c89ba6cd3aa67598092fcc736d4119e
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-10 08:48:52 +0530

    #64 : Parsing incoming requests part 1

--- Sec_5:_Working_with_Expressjs/01-adding-middleware/app.js
+++ Sec_5:_Working_with_Expressjs/01-adding-middleware/app.js
@@ -11,8 +11,14 @@ app.use((req,res,next)=>{
 
 // add-product middleware.
 app.use('/add-product',(req, res, next) => {
-    console.log('In the products middleware!');
-    res.send('<h1 style="color:red;">The add product page </h1>');
+    console.log('In the add-product middleware!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit" >Add Product</button></form>');
+});
+// product middleware.
+app.use('/product',(req, res, next) => {
+    console.log('In the product middleware!');
+    console.log(req.body) // without a body parser this will give undefined
+    res.redirect('/');
 });
 
 // root path.
