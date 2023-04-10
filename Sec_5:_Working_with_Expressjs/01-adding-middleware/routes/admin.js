d740082b95b0781745d5fc013bbdf337a2f0206d
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-11 00:28:57 +0530

    #68 filtering paths

--- Sec_5:_Working_with_Expressjs/01-adding-middleware/routes/admin.js
+++ Sec_5:_Working_with_Expressjs/01-adding-middleware/routes/admin.js
@@ -3,12 +3,14 @@ const express = require('express')
 const router = express.Router();
 
 // add-product middleware.
+// /admin/add-product ==> GET
 router.get('/add-product',(req, res, next) => {
     console.log('In the add-product middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit" >Add Product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"></input><button type="submit" >Add Product</button></form>');
 });
 // product middleware.
 // here this will only trigger only for post requests
+// /admin/add-product ==> POST
 router.post('/add-product',(req, res, next) => {
     console.log('In the product middleware!');
     console.log(req.body) // without a body parser this will give undefined
