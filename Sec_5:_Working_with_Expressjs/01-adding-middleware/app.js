7c5212a519ecd8a3840a5c8ad66b00a33c02e6d7
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-10 23:51:33 +0530

    integrated the exported adminRoutes from the admin.js to app.js

--- Sec_5:_Working_with_Expressjs/01-adding-middleware/app.js
+++ Sec_5:_Working_with_Expressjs/01-adding-middleware/app.js
@@ -6,7 +6,13 @@ const bodyParser = require('body-Parser');
 
 const app = express();
 
+const adminRoutes = require("./routes/admin")
+
 app.use(bodyParser.urlencoded({extended:false}))
+
+// here also order matters 
+// here we are just using the adminRoutes object exported
+app.use(adminRoutes);
 
 // root path.
 app.use('/',(req, res, next) => {
