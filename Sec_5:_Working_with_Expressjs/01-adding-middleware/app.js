5fb4f0821c3b916e454a87477f9819f88e490c44
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-11 00:05:10 +0530

    #67 : adding a 404 Page

--- Sec_5:_Working_with_Expressjs/01-adding-middleware/app.js
+++ Sec_5:_Working_with_Expressjs/01-adding-middleware/app.js
@@ -17,6 +17,10 @@ app.use(adminRoutes);
 
 app.use(shopRoutes);
 
+app.use((req, res, next)=>{
+    res.status(404).send("<h1 style='color:#fe0000c7;'> Page not found </h1>")
+})
+
 const server = http.createServer(app);
 
 server.listen(3000);
