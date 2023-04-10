39acd81abe2ebbef09492f0dca1a7c57466e8ea4
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-10 23:47:04 +0530

    shifted add-product and product middlewares to admin.js

--- Sec_5:_Working_with_Expressjs/01-adding-middleware/app.js
+++ Sec_5:_Working_with_Expressjs/01-adding-middleware/app.js
@@ -8,19 +8,6 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}))
 
-// add-product middleware.
-app.use('/add-product',(req, res, next) => {
-    console.log('In the add-product middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit" >Add Product</button></form>');
-});
-// product middleware.
-// here this will only trigger only for post requests
-app.post('/product',(req, res, next) => {
-    console.log('In the product middleware!');
-    console.log(req.body) // without a body parser this will give undefined
-    res.redirect('/');
-});
-
 // root path.
 app.use('/',(req, res, next) => {
     console.log('In another middleware!');
