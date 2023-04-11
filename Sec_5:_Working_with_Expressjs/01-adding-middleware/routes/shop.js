c30d44e08f48695703b86fc10a0beaf319068d92
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-12 01:12:02 +0530

    made the path relative to serve html pages

--- Sec_5:_Working_with_Expressjs/01-adding-middleware/routes/shop.js
+++ Sec_5:_Working_with_Expressjs/01-adding-middleware/routes/shop.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
@@ -5,7 +7,13 @@ const router = express.Router();
 // root path.
 router.get('/',(req, res, next) => {
     console.log('In another middleware!');
-    res.send('<h1 style="color:red;">Hello from Express!</h1>');
+    res.sendFile(path.join(__dirname,'..','views','shop.html'));
+    // args in sendfile above
+    // 1. __dirname which is a global variable provided by nodejs for us to avoid OS related path gocchas.
+    // 2. '..' this tells to go up one level and then search for views cause we are using this middleware inside the app.js so the path should be relative to that file (app.js). 
+    // 3. 'views' name of the directory.
+    // 4. 'shop.html' is name of the file.
+    // NOTE : here we are not using the / or \ for traversing the file system because in windows we use '\' for traversing and in linux and mac we use '/' for traversing.
 });
 
 module.exports = router
