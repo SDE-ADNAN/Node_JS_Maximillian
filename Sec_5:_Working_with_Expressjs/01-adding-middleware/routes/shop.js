cba93d99fcac3569599ca19e52d68047c70977e2
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-14 01:06:18 +0530

    consumed rootdir

--- Sec_5:_Working_with_Expressjs/01-adding-middleware/routes/shop.js
+++ Sec_5:_Working_with_Expressjs/01-adding-middleware/routes/shop.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
+const rootDir = require('../util/path')
+
 const express = require("express");
 
 const router = express.Router();
@@ -7,7 +9,7 @@ const router = express.Router();
 // root path.
 router.get('/',(req, res, next) => {
     console.log('In another middleware!');
-    res.sendFile(path.join(__dirname,'..','views','shop.html'));
+    res.sendFile(path.join(rootDir,'views','shop.html'));
     // args in sendfile above
     // 1. __dirname which is a global variable provided by nodejs for us to avoid OS related path gocchas.
     // 2. '..' this tells to go up one level and then search for views cause we are using this middleware inside the app.js so the path should be relative to that file (app.js). 
