48017d727feb41d1bfdd9854b4654ab1a1aced8d
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-19 23:21:27 +0530

    added port info on console.

--- Sec_6:_Working_with_Dynamic_Content_&_Adding_Templating_Engines/01-sharing-data-across-requests-and-users/app.js
+++ Sec_6:_Working_with_Dynamic_Content_&_Adding_Templating_Engines/01-sharing-data-across-requests-and-users/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const port = 3000;
 
 // this below line will set the templating engine as pug // config item
 app.set('view engine','pug');
@@ -22,5 +23,5 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
-app.listen(3000);
+console.warn(`Running at : ${port}`)
+app.listen(port);
