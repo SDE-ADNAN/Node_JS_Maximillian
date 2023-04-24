95b2befef899b8e6056d168748ae409dce83b33b
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-24 09:46:09 +0530

    made configurations for the hbs main-layout

--- Sec_6:_Working_with_Dynamic_Content_&_Adding_Templating_Engines/01-sharing-data-across-requests-and-users/app.js
+++ Sec_6:_Working_with_Dynamic_Content_&_Adding_Templating_Engines/01-sharing-data-across-requests-and-users/app.js
@@ -1,28 +1,31 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars')
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 const port = 3000;
 
-app.engine('hbs' , expressHbs()) //  we need to use this type of import for the templating engine which does'nt sheeps along with  express ( in case of pug ,  it was directly shipped along with express )
-app.set('view engine','hbs');
-app.set('views','views')
+app.engine(
+  "hbs",
+  expressHbs({ layoutsDir: "views/layouts", defaultLayout: "main-layout" , extname:'hbs'})
+); //  we need to use this type of import for the templating engine which does'nt sheeps along with  express ( in case of pug ,  it was directly shipped along with express )
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.render("404",{pageTitle:"Page Not Found !!"})
+  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.render("404", { pageTitle: "Page Not Found !!" });
 });
-console.warn(`Running at : ${port}`)
+console.warn(`Running at : ${port}`);
 app.listen(port);
