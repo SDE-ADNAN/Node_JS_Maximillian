da5b20f65cdc31090443504e718048618936e9a7
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-29 22:03:46 +0530

    passed the path as "" which was causing issues while rendering 404 page

--- Sec_6:_Working_with_Dynamic_Content_&_Adding_Templating_Engines/01-sharing-data-across-requests-and-users/app.js
+++ Sec_6:_Working_with_Dynamic_Content_&_Adding_Templating_Engines/01-sharing-data-across-requests-and-users/app.js
@@ -20,7 +20,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  res.render("404", { pageTitle: "Page Not Found !!" });
+  res.render("404", { path:"",pageTitle: "Page Not Found !!" });
 });
 console.warn(`Running at : ${port}`);
 app.listen(port);
