d3eddd34d5160522f3327e3b66a0bdcbfb945ac3
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-05-11 06:35:41 +0530

    sec 9 Dynamic routing and advanced models

--- Sec_9:_Dynamic_routes_and_advance_models/app.js
+++ Sec_9:_Dynamic_routes_and_advance_models/app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -10,7 +12,6 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,7 +21,4 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-const port = 3000;
-
-app.listen(port);
-console.log(`started on ${port}`)
+app.listen(3000);
