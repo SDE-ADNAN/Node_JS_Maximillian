6f8e706e55b14af38512fd54259dd6f301301529
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-06-06 23:26:11 +0530

    used find by id controller

--- Sec_9:_Dynamic_routes_and_advance_models/controllers/shop.js
+++ Sec_9:_Dynamic_routes_and_advance_models/controllers/shop.js
@@ -12,7 +12,10 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodID = req.params.productId;
-  console.log("productId   "+prodID);
+  // console.log("productId   "+prodID);
+  Product.findById(prodID , (product)=>{
+    console.log(product)
+  })
   res.redirect('/')
 };
 
